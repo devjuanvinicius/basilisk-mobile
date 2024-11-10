ee2d2fdc3f3fee3a1b8e074a588b62efe6394879
Juan Vinicius <dev.juanvinicius@gmail.com>
2024-11-10 14:59:36 -0300

    refix: refatoração de estilo das telas de cadastro

--- app/src/main/java/com/example/dashboardbasi/CadastroActivity.kt
+++ app/src/main/java/com/example/dashboardbasi/CadastroActivity.kt
@@ -1,20 +1,21 @@
 package com.example.dashboardbasi
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentActivity
 
-/**
- * Loads [MainFragment].
- */
 class CadastroActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_browse_fragment, MainFragment())
-                .commitNow()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainBackgroundCadastro)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
