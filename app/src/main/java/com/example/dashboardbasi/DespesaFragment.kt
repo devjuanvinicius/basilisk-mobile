3bead411f97c3b2fc33ae768308509d47af84450
Iaanzen <iaaanzen@gmail.com>
2024-11-19 18:08:50 -0300

    Revert "Revert "Feat: Adição de telas de renda e despesa e refatoração de telas e deleção do botao config""
    This reverts commit 622a59c1d8898216d38eb93da73c2da46f9ea94c.

--- app/src/main/java/com/example/dashboardbasi/DespesaFragment.kt
+++ app/src/main/java/com/example/dashboardbasi/DespesaFragment.kt
@@ -1,15 +1,27 @@
 package com.example.dashboardbasi
 
-import android.graphics.Typeface
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class DespesaFragment : Fragment(R.layout.fragment_despesa) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Configurando botões para chamar as funções
+        view.findViewById<View>(R.id.novaDespesa)?.setOnClickListener {
+            irParaAddDespesa()
+        }
+
+
+    }
+
+    private fun irParaAddDespesa() {
+        val intent = Intent(requireContext(), novaDespesa::class.java)
+        startActivity(intent)
+    }
 
 
 }
--- app/src/main/java/com/example/dashboardbasi/RendaFragment.kt
+++ app/src/main/java/com/example/dashboardbasi/RendaFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dashboardbasi
 
+import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
@@ -11,5 +12,17 @@ import androidx.fragment.app.Fragment
 
 class RendaFragment : Fragment(R.layout.fragment_renda) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Exemplo de uso da função: você pode vinculá-la a um botão ou chamá-la diretamente.
+        view.findViewById<View>(R.id.editarRenda)?.setOnClickListener {
+            irParaRendas()
+        }
+    }
+
+    private fun irParaRendas() {
+        val intent = Intent(requireContext(), TotalRendas::class.java)
+        startActivity(intent)
+    }
 }
