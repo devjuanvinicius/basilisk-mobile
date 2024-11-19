7ad3255ed2573ddb09a628ce8d9ffd879a92196d
leticiaMarquesAPereira <leticia.marques.a.p05@gmail.com>
2024-11-19 19:28:36 -0300

    Fix: Mudança no visual do SearchView

--- app/src/main/java/com/example/dashboardbasi/InvestimentoActivity.kt
+++ app/src/main/java/com/example/dashboardbasi/InvestimentoActivity.kt
@@ -13,22 +13,30 @@ class InvestimentoActivity : AppCompatActivity() {
 
   lateinit var homeButton: ImageButton
 
+  // Declare apenas a variável searchView
+  private lateinit var searchView: SearchView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContentView(R.layout.activity_investimento)
+
+    // Corrigindo a inicialização do searchView
+    searchView = findViewById(R.id.searchView)
+
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
     }
 
-    homeButton = findViewById(R.id.homeButton)
-    homeButton.setOnClickListener{
-      val intent = Intent(this, MainActivity::class.java)
+    searchView.onActionViewExpanded() // Expande o SearchView automaticamente
+    searchView.clearFocus()
 
+    homeButton = findViewById(R.id.homeButton)
+    homeButton.setOnClickListener {
+      val intent = Intent(this, MainActivity::class.java)
       startActivity(intent)
     }
   }
-  private lateinit var searchView: SearchView
 }
