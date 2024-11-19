622a59c1d8898216d38eb93da73c2da46f9ea94c
Iaanzen <iaaanzen@gmail.com>
2024-11-19 18:04:44 -0300

    Revert "Feat: Adição de telas de renda e despesa e refatoração de telas e deleção do botao config"
    This reverts commit 0611200afefb112b863c4c02fac2a262e8c104bc.

--- app/src/main/java/com/example/dashboardbasi/RendaFragment.kt
+++ app/src/main/java/com/example/dashboardbasi/RendaFragment.kt
@@ -1,6 +1,5 @@
 package com.example.dashboardbasi
 
-import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
@@ -12,17 +11,5 @@ import androidx.fragment.app.Fragment
 
 class RendaFragment : Fragment(R.layout.fragment_renda) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        // Exemplo de uso da função: você pode vinculá-la a um botão ou chamá-la diretamente.
-        view.findViewById<View>(R.id.editarRenda)?.setOnClickListener {
-            irParaRendas()
-        }
-    }
-
-    private fun irParaRendas() {
-        val intent = Intent(requireContext(), TotalRendas::class.java)
-        startActivity(intent)
-    }
 }
