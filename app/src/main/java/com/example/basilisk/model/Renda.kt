2f865f082b41cf117aa3f68efb1aa35e8701fec5
devjuanvinicius <dev.juanvinicius@gmail.com>
2024-11-22 17:30:54 -0300

    feat: criação do DAO e classe de renda e de investimento.

---
+++ app/src/main/java/com/example/basilisk/model/Renda.kt
@@ -0,0 +1,9 @@
+package com.example.basilisk.model
+
+data class Renda(
+    val id: String,
+    val nome: String,
+    val valor: Double,
+    val dataRecebimento: String,
+    val rendaFixa: Boolean
+)
