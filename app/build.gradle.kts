137eb4b672867584c0e241784b6c387ff7f3bbcf
devjuanvinicius <dev.juanvinicius@gmail.com>
2024-11-23 21:47:19 -0300

    fix: correção do arquivo de gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.basilisk"
-        minSdk = 33
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -36,30 +36,41 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-    viewBinding{
-        enable = true
     }
 }
 
 dependencies {
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-analytics")
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    // AndroidX e Material
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // MPAndroidChart
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10")
+
+    // API Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+
+    // Fragment KTX (versão mais recente)
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    // Testes
     testImplementation(libs.junit)
-    implementation ("androidx.fragment:fragment-ktx:1.5.0")
-    //api
-    implementation(libs.retrofit)
-    implementation(libs.gson)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
